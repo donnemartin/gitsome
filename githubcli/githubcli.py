c38b570888262f25ce1f89f85d4b41aa424594d0
Donne Martin <donne.martin@gmail.com>
2015-11-10 07:14:55 -0500

    Start rewriting the class that interacts with GitHub through CLI.

---
+++ githubcli/githubcli.py
@@ -0,0 +1,44 @@
+# coding: utf-8
+import builtins
+from operator import itemgetter
+import os
+import pickle
+import re
+import subprocess
+import sys
+import webbrowser
+
+import click
+from github3 import login, null
+from tabulate import tabulate
+from gitsome.gitsome_command import GitSomeCommand
+from xonsh.built_ins import iglobpath
+from xonsh.environ import repo_from_remote
+from xonsh.tools import subexpr_from_unbalanced
+from xonsh.tools import ON_WINDOWS
+
+
+class GitHub(object):
+    """Provides integration with the GitHub API.
+
+    Attributes:
+        * api: An instance of github3 to interact with the GitHub API.
+        * repo: A string that represents the user's current repo, as
+            determined by the .git/ configured remote repo.
+        * user_id: A string that represents the user's id in ~/.xonshrc
+        * user_pass: A string that represents the user's pass in ~/.xonshrc
+        * user_token: A string that represents the user's token in ~/.xonshrc
+
+    """
+
+    def __init__(self):
+        """Inits GitSome.
+
+        Args:
+            * None.
+
+        Returns:
+            None.
+        """
+        self._login()
+        self.repo = repo_from_remote()
