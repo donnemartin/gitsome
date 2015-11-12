3f624ef624b297607c059dc81bd945685606b22e
Donne Martin <donne.martin@gmail.com>
2015-11-12 06:20:05 -0500

    Fix _login imports.

--- githubcli/githubcli.py
+++ githubcli/githubcli.py
@@ -7,6 +7,7 @@ import re
 import subprocess
 import sys
 import webbrowser
+from getpass import getpass
 try:
     # Python 3
     import configparser
@@ -15,7 +16,8 @@ except ImportError:
     import ConfigParser as configparser
 
 import click
-from github3 import login, null
+from github3 import authorize, login, null
+from github3.exceptions import UnprocessableEntity
 from tabulate import tabulate
 from gitsome.gitsome_command import GitSomeCommand
 from xonsh.built_ins import iglobpath
