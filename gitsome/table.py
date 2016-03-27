15d0a65c9f149024cfc6eba262bb58419a346598
Donne Martin <donne.martin@gmail.com>
2016-03-27 07:55:36 -0400

    Add build_table method

--- gitsome/table.py
+++ gitsome/table.py
@@ -35,3 +35,34 @@ class Table(object):
             None.
         """
         self.config = config
+
+    def build_table(self, view_entries, limit, format_method):
+        """Builds the table and headers with tabulate.
+
+        Args:
+            * view_entries: xxx.
+            * limit: An int that specifies the number of items to show.
+            * format_method: A method called to format each item in the table.
+
+        Returns:
+            None.
+        """
+        self.build_table_urls(view_entries)
+        index = 0
+        for view_entry in view_entries:
+            index += 1
+            view_entry.index = index
+            click.echo(format_method(view_entry))
+            if index >= limit:
+                break
+        if len(view_entries) > limit:
+            click.secho(('  Hiding ' +
+                         str(len(view_entries) - limit) +
+                         ' item(s), use -l/--limit ' +
+                         str(len(view_entries)) +
+                         ' to view all items.'),
+                        fg='magenta')
+        if index == 0:
+            click.secho('No results found', fg=None)
+        else:
+            click.secho(self.create_tip(index))
