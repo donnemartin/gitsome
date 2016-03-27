a3349257fab91e57f86c7d68973b1c49f00f31b3
Donne Martin <donne.martin@gmail.com>
2016-03-27 07:38:25 -0400

    Add Formatter class with format_issues_url_from_issue

---
+++ gitsome/formatter.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+
+# -*- coding: utf-8 -*-
+
+# Copyright 2015 Donne Martin. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+import click
+from .lib.pretty_date_time import pretty_date_time
+
+
+class Formatter(object):
+
+    def format_issues_url_from_issue(self, issue):
+        """Formats the issue url based on the given issue.
+
+        Args:
+            * thread: An instance of github3.issues.Issue.
+
+        Returns:
+            A string representing the formatted issues url.
+        """
+        return self.format_user_repo(issue.repository) + '/' + \
+            'issues/' + str(issue.number)
