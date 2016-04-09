c03112d429d774e0ff604012d9321813a58726d4
Donne Martin <donne.martin@gmail.com>
2016-04-09 09:04:48 -0400

    Add expected email test result

---
+++ tests/data/email.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2015 Donne Martin. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+formatted_emails = u'\x1b[35m  1.   \x1b[0mfoo@baz.com \x1b[0m\n\x1b[32m        Primary: True    \x1b[0m\x1b[36mVerified: False \x1b[0m\n\x1b[35m  2.   \x1b[0mbar@baz.com \x1b[0m\n\x1b[32m        Primary: False   \x1b[0m\x1b[36mVerified: True  \x1b[0m\n\x1b[0m'  # NOQA
