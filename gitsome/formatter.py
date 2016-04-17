c7f57de66eb51e8d333fc98d58dbcc874136acce
Donne Martin <donne.martin@gmail.com>
2016-04-16 22:20:55 -0400

    Update formatters to use color config

--- gitsome/formatter.py
+++ gitsome/formatter.py
@@ -25,20 +25,24 @@ class Formatter(object):
     """Handles formatting of isssues, repos, threads, etc.
 
     Attributes:
+        * config: An instance of config.Config.
+        * event_handlers: A mapping of raw event types to format methods.
         * event_type_mapping: A mapping of raw event types to more
             human readable text.
             See: https://developer.github.com/v3/activity/events/types/
+        * pretty_dt: An instance of pretty_date_time
     """
 
-    def __init__(self):
+    def __init__(self, config):
         """Inits Formatter.
 
         Args:
-            * None.
+            * config: An instance of config.Config.
 
         Returns:
             None.
         """
+        self.config = config
         self.event_type_mapping = {
             'CommitCommentEvent': 'commented on commit',
             'CreateEvent': 'created',
@@ -82,25 +86,26 @@ class Formatter(object):
             'RepositoryEvent': self._format_general_event,
             'WatchEvent': self._format_general_event,
         }
+        self.pretty_dt = pretty_date_time
 
     def _format_time(self, event):
         item = click.style(
-            ' (' + str(pretty_date_time(event.created_at)) + ')',
-            fg='yellow')
+            ' (' + str(self.pretty_dt(event.created_at)) + ')',
+            fg=self.config.clr_time)
         return item
 
     def _format_issue_comment(self, event, key):
         issue = '{repo[0]}/{repo[1]}#{num}'.format(
             repo=event.payload[key].repository,
             num=event.payload[key].number)
-        return click.style(issue, fg='cyan')
+        return click.style(issue, fg=self.config.clr_tertiary)
 
     def _format_commit_or_comment(self, message, sha=''):
         indent = '         '
         subsequent_indent = indent if sha == '' else '                  '
         message = self.strip_line_breaks(message)
         formatted_message = click.wrap_text(
-            text=click.style(sha, fg='cyan')+message,
+            text=click.style(sha, fg=self.config.clr_tertiary)+message,
             initial_indent=indent,
             subsequent_indent=subsequent_indent)
         return formatted_message
@@ -110,75 +115,87 @@ class Formatter(object):
 
     def _format_commit_comment_event(self, event):
         item = click.style(self.event_type_mapping[event.type] + ' ',
-                           fg='green')
+                           fg=self.config.clr_secondary)
         item += click.style(
             self._format_sha(event.payload['comment'].commit_id),
-            fg='cyan')
-        item += click.style(' at ', fg='green')
-        item += click.style(self.format_user_repo(event.repo), fg='cyan')
+            fg=self.config.clr_tertiary)
+        item += click.style(' at ', fg=self.config.clr_secondary)
+        item += click.style(self.format_user_repo(event.repo),
+                            fg=self.config.clr_tertiary)
         item += self._format_time(event)
         item += click.style('\n')
         message = self._format_commit_or_comment(event.payload['comment'].body)
-        item += click.style(message, fg=None)
+        item += click.style(message, fg=self.config.clr_message)
         return item
 
     def _format_create_delete_event(self, event):
-        item = click.style(self.event_type_mapping[event.type], fg='green')
-        item += click.style(' ' + event.payload['ref_type'], fg='green')
+        item = click.style(self.event_type_mapping[event.type],
+                           fg=self.config.clr_secondary)
+        item += click.style(' ' + event.payload['ref_type'],
+                            fg=self.config.clr_secondary)
         if event.payload['ref']:
-            item += click.style(' ' + event.payload['ref'], fg='cyan')
-        item += click.style(' at ', fg='green')
-        item += click.style(self.format_user_repo(event.repo), fg='cyan')
+            item += click.style(' ' + event.payload['ref'],
+                                fg=self.config.clr_tertiary)
+        item += click.style(' at ', fg=self.config.clr_secondary)
+        item += click.style(self.format_user_repo(event.repo),
+                            fg=self.config.clr_tertiary)
         item += self._format_time(event)
         return item
 
     def _format_fork_event(self, event):
-        item = click.style(self.event_type_mapping[event.type], fg='green')
-        item += click.style(' ' + self.format_user_repo(event.repo), fg='cyan')
+        item = click.style(self.event_type_mapping[event.type],
+                           fg=self.config.clr_secondary)
+        item += click.style(' ' + self.format_user_repo(event.repo),
+                            fg=self.config.clr_tertiary)
         item += self._format_time(event)
         return item
 
     def _format_issue_commment_event(self, event):
         item = click.style(self.event_type_mapping[event.type] + ' ',
-                           fg='green')
+                           fg=self.config.clr_secondary)
         item += self._format_issue_comment(event, key='issue')
         item += self._format_time(event)
         item += click.style('\n')
         message = self._format_commit_or_comment(event.payload['comment'].body)
-        item += click.style(message, fg=None)
+        item += click.style(message, fg=self.config.clr_message)
         return item
 
     def _format_issues_event(self, event):
-        item = click.style(event.payload['action'] + ' issue ', fg='green')
+        item = click.style(event.payload['action'] + ' issue ',
+                           fg=self.config.clr_secondary)
         item += self._format_issue_comment(event, key='issue')
         item += self._format_time(event)
         return item
 
     def _format_pull_request_event(self, event):
         item = click.style(event.payload['action'] + ' pull request ',
-                           fg='green')
+                           fg=self.config.clr_secondary)
         item += self._format_issue_comment(event, key='pull_request')
         item += self._format_time(event)
         return item
 
     def _format_push_event(self, event):
-        item = click.style(self.event_type_mapping[event.type], fg='green')
+        item = click.style(self.event_type_mapping[event.type],
+                           fg=self.config.clr_secondary)
         branch = event.payload['ref'].split('/')[-1]
-        item += click.style(' ' + branch, fg='cyan')
-        item += click.style(' at ', fg='green')
-        item += click.style(self.format_user_repo(event.repo), fg='cyan')
+        item += click.style(' ' + branch, fg=self.config.clr_tertiary)
+        item += click.style(' at ', fg=self.config.clr_secondary)
+        item += click.style(self.format_user_repo(event.repo),
+                            fg=self.config.clr_tertiary)
         item += self._format_time(event)
         for commit in event.payload['commits']:
             item += click.style('\n')
-            sha = click.style(self._format_sha(commit['sha']) + ': ', fg=None)
+            sha = click.style(self._format_sha(commit['sha']) + ': ',
+                              fg=self.config.clr_message)
             message = self._format_commit_or_comment(commit['message'], sha=sha)
-            item += click.style(message, fg=None)
+            item += click.style(message, fg=self.config.clr_message)
         return item
 
     def _format_general_event(self, event):
         item = click.style(self.event_type_mapping[event.type] + ' ',
-                           fg='green')
-        item += click.style(self.format_user_repo(event.repo), fg='cyan')
+                           fg=self.config.clr_secondary)
+        item += click.style(self.format_user_repo(event.repo),
+                            fg=self.config.clr_tertiary)
         item += self._format_time(event)
         return item
 
@@ -196,10 +213,10 @@ class Formatter(object):
         item += '\n'
         item += click.style(('        ' + 'Primary: ' +
                              str(email.primary).ljust(7) + ' '),
-                            fg='green')
+                            fg=self.config.clr_secondary)
         item += click.style(('Verified: ' +
                              str(email.verified).ljust(5) + ' '),
-                            fg='cyan')
+                            fg=self.config.clr_tertiary)
         return item
 
     def format_emoji(self, view_entry):
@@ -259,14 +276,14 @@ class Formatter(object):
         repo = item_parts[-1]
         item = self.format_index_title(view_entry.index, title)
         if action[0] == 'forked':
-            item += click.style(action[0] + ' ', fg='green')
-            item += click.style(action[1] + ' ', fg='cyan')
+            item += click.style(action[0] + ' ', fg=self.config.clr_secondary)
+            item += click.style(action[1] + ' ', fg=self.config.clr_tertiary)
         else:
-            item += click.style(' '.join(action), fg='green')
-            item += click.style(' ' + repo + ' ', fg='cyan')
+            item += click.style(' '.join(action), fg=self.config.clr_secondary)
+            item += click.style(' ' + repo + ' ', fg=self.config.clr_tertiary)
         item += click.style(
-            '(' + str(pretty_date_time(feed_entry.updated_parsed)) + ')',
-            fg='yellow')
+            '(' + str(self.pretty_dt(feed_entry.updated_parsed)) + ')',
+            fg=self.config.clr_time)
         if action[0] == 'commented':
             comment_parts = feed_entry['summary'].split('blockquote')
             if len(comment_parts) > 2:
@@ -280,7 +297,7 @@ class Formatter(object):
                 comment = re.sub(r'(</p>*)', r'', comment)
                 comment = re.sub(r'(</*)', r'', comment)
                 comment = re.sub(r'(>      *)', r'', comment)
-                item += click.style('\n' + comment, fg=None)
+                item += click.style('\n' + comment, fg=self.config.clr_message)
         return item
 
     def format_license_name(self, view_entry):
@@ -295,7 +312,8 @@ class Formatter(object):
         license_template_name = view_entry.item
         item = self.format_index_title(view_entry.index,
                                        license_template_name.key)
-        item += click.style('(' + license_template_name.name + ')', fg=None)
+        item += click.style('(' + license_template_name.name + ')',
+                            fg=self.config.clr_secondary)
         return item
 
     def format_user(self, view_entry):
@@ -350,9 +368,9 @@ class Formatter(object):
             A string representation of the formatted index and title.
         """
         formatted_index_title = click.style('  ' + (str(index) + '.').ljust(5),
-                                            fg='magenta')
+                                            fg=self.config.clr_view_index)
         formatted_index_title += click.style(title + ' ',
-                                             fg='white')
+                                             fg=self.config.clr_primary)
         return formatted_index_title
 
     def format_issue(self, view_entry):
@@ -366,27 +384,28 @@ class Formatter(object):
         """
         issue = view_entry.item
         item = self.format_index_title(view_entry.index, issue.title)
-        item += click.style('@' + str(issue.user) + ' ', fg='white')
+        item += click.style('@' + str(issue.user) + ' ',
+                            fg=self.config.clr_primary)
         item += click.style(('(' +
                              self.format_issues_url_from_issue(issue) +
                              ')'),
-                            fg='magenta')
+                            fg=self.config.clr_view_link)
         item += '\n'
         indent = '        '
         if len(item) == 8:
             item += click.style(('        Score: ' +
                                  str(item[7]).ljust(10) + ' '),
-                                fg='yellow')
+                                fg=self.config.clr_quaternary)
             indent = '  '
         item += click.style((indent + 'State: ' +
                              str(issue.state).ljust(10) + ' '),
-                            fg='green')
+                            fg=self.config.clr_secondary)
         item += click.style(('Comments: ' +
                              str(issue.comments_count).ljust(5) + ' '),
-                            fg='cyan')
+                            fg=self.config.clr_tertiary)
         item += click.style(('Assignee: ' +
                              str(issue.assignee).ljust(10) + ' '),
-                            fg='yellow')
+                            fg=self.config.clr_quaternary)
         return item
 
     def format_repo(self, view_entry):
@@ -402,16 +421,16 @@ class Formatter(object):
         item = self.format_index_title(view_entry.index, repo.full_name)
         language = repo.language if repo.language is not None else 'Unknown'
         item += click.style('(' + language + ')',
-                            fg=None)
+                            fg=self.config.clr_message)
         item += '\n'
         item += click.style(('        ' + 'Stars: ' +
                              str(repo.stargazers_count).ljust(5) + ' '),
-                            fg='green')
+                            fg=self.config.clr_secondary)
         item += click.style('Forks: ' + str(repo.forks_count).ljust(4) + ' ',
-                            fg='cyan')
+                            fg=self.config.clr_tertiary)
         item += click.style(('Updated: ' +
-                             str(pretty_date_time(repo.updated_at)) + ' '),
-                            fg='yellow')
+                             str(self.pretty_dt(repo.updated_at)) + ' '),
+                            fg=self.config.clr_time)
         return item
 
     def format_thread(self, view_entry):
@@ -427,17 +446,17 @@ class Formatter(object):
         item = self.format_index_title(view_entry.index,
                                        thread.subject['title'])
         item += click.style('(' + view_entry.url + ')',
-                            fg='magenta')
+                            fg=self.config.clr_view_link)
         item += '\n'
         item += click.style(('        ' + 'Seen: ' +
                              str(not thread.unread).ljust(7) + ' '),
-                            fg='green')
+                            fg=self.config.clr_secondary)
         item += click.style(('Type: ' +
                              str(thread.subject['type']).ljust(12) + ' '),
-                            fg='cyan')
+                            fg=self.config.clr_tertiary)
         item += click.style(('Updated: ' +
-                             str(pretty_date_time(thread.updated_at)) + ' '),
-                            fg='yellow')
+                             str(self.pretty_dt(thread.updated_at)) + ' '),
+                            fg=self.config.clr_time)
         return item
 
     def format_trending_entry(self, view_entry):
@@ -462,20 +481,21 @@ class Formatter(object):
         language = re.sub(r'(\()', r'', language)
         language = re.sub(r'(\))', r'', language)
         item += click.style(
-            '(' + str(pretty_date_time(trending_entry.updated_parsed)) + ')',
-            fg='yellow')
+            '(' + str(self.pretty_dt(trending_entry.updated_parsed)) + ')',
+            fg=self.config.clr_time)
         if summary:
             item += '\n'
             summary = click.wrap_text(
                 text=summary,
                 initial_indent='         ',
                 subsequent_indent='         ')
-        item += click.style(summary, fg=None)
+        item += click.style(summary, self.config.clr_message)
         item += '\n'
-        item += click.style('         Language: ' + language, fg='green')
+        item += click.style('         Language: ' + language,
+                            fg=self.config.clr_secondary)
         return item
 
-    def format_user_repo(self, repo):
+    def format_user_repo(self, user_repo_tuple):
         """Formats a repo tuple for pretty print.
 
         Example:
@@ -485,12 +505,12 @@ class Formatter(object):
             Output: donnemartin/gitsome
 
         Args:
-            * args: A tuple that contains the user and repo.
+            * user_repo_tuple: A tuple that contains the user and repo.
 
         Returns:
             A string of the form user/repo.
         """
-        result = '/'.join(repo)
+        result = '/'.join(user_repo_tuple)
         if result.startswith('repos/'):
             return result[len('repos/'):]
         return result
