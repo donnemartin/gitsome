fccd09fad7fd7c4bf784e7bff2e5ffa1391185fe
Donne Martin <donne.martin@gmail.com>
2016-04-18 05:36:30 -0400

    Remove events command
    Will be replaced by a revamped feed command.

--- gitsome/githubcli.py
+++ gitsome/githubcli.py
@@ -173,31 +173,6 @@ class GitHubCli(object):
 
     @cli.command()
     @pass_github
-    def events(github):
-        """Lists all public events.
-
-        Examples:
-            gh events | grep foo
-            gh events | less
-
-        Args:
-            * None.
-
-        Returns:
-            None.
-        """
-        events = github.api.all_events()
-        table = []
-        for event in events:
-            table.append([event.created_at,
-                          event.actor,
-                          event.type,
-                          github.format_repo(event.repo)])
-        github.print_table(table,
-                           headers=['created at', 'user', 'type', 'repo'])
-
-    @cli.command()
-    @pass_github
     def feeds(github):
         """Lists GitHub's timeline resources.
 
