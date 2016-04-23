30d39dd0813b0c784ceff6cdc4b1df28f5a9ae9f
Donne Martin <donne.martin@gmail.com>
2016-04-22 22:42:58 -0400

    Add MockUser class

--- tests/mock_github_api.py
+++ tests/mock_github_api.py
@@ -13,6 +13,10 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
+import mock
+
+from gitsome.lib.github3.exceptions import UnprocessableEntity
+
 
 class MockEmail(object):
 
@@ -20,3 +24,34 @@ class MockEmail(object):
         self.email = email
         self.primary = primary
         self.verified = verified
+
+
+class MockUser(object):
+
+    def __init__(self, login, user_type='User'):
+        self.login = login
+        self.repositories = {}
+        self.emails = []
+        self.avatar_url = 'https://www.github.com'
+        self.company = 'bigcorp'
+        self.location = 'interwebz'
+        self.email = login + '@foo.com'
+        self.type = user_type
+        self.followers_count = 0
+        self.following_count = 0
+
+    def raise_mock_unprocessableentity(self):
+        response = mock.Mock()
+        response.json = lambda: exec('raise(Exception())')
+        response.content = 'foobar'
+        raise UnprocessableEntity(response)
+
+    def create_repo(self, name, desc='', private=False):
+        if name in self.repositories:
+            self.raise_mock_unprocessableentity()
+        repo = MockRepo(self, name, desc, private)
+        self.repositories.update({repo.full_name: repo})
+        return repo
+
+    def events(self, public):
+        return []
