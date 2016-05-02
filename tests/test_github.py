a70b45563d0456a248e7f3744a4d7b23d3872a6e
Donne Martin <donne.martin@gmail.com>
2016-05-02 06:34:34 -0400

    Update Python future imports
    Possible future Python 2 compatibility.

--- tests/test_github.py
+++ tests/test_github.py
@@ -13,8 +13,8 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
+from __future__ import unicode_literals
 from __future__ import print_function
-from __future__ import division
 
 import mock
 
