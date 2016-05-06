2b74c4c7b6034645ede7279aee0eeae35983d16a
Donne Martin <donne.martin@gmail.com>
2016-05-05 20:16:28 -0400

    Fix test_search_repos method to assert on the sort param

--- tests/test_github.py
+++ tests/test_github.py
@@ -250,7 +250,7 @@ class GitHubTest(unittest.TestCase):
     def test_search_repos(self, mock_github_repositories, mock_click_secho):
         self.github.search_repositories('foo', 'stars')
         mock_github_repositories.assert_called_with(
-            ['foobar'], 1000, False)
+            ['foobar'], 1000, False, sort=False)
 
     @mock.patch('gitsome.github.click.secho')
     def test_trending(self, mock_click_secho):
