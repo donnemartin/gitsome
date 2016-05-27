9811dbc4f1d0d99d23676ea6b6e1f004e1f50c5a
Donne Martin <donne.martin@gmail.com>
2016-05-27 06:32:29 -0400

    Update packaging deps upper bound based on semantic versioning

--- setup.py
+++ setup.py
@@ -99,17 +99,17 @@ def main():
     if HAVE_SETUPTOOLS:
         skw['setup_requires'] = ['ply']
         skw['install_requires'] = [
-            'numpydoc==0.5',
-            'ply==3.4',
+            'numpydoc>=0.5,<1.0',
+            'ply>=3.4,<4.0',
             'prompt-toolkit==0.52',
-            'requests==2.8.1',
-            'colorama>=0.3.3',
-            'click>=5.1',
-            'pygments>=2.0.2',
-            'feedparser>=5.2.1',
-            'pytz>=2016.3',
-            'docopt==0.6.2',
-            'uritemplate.py>=0.2.0',
+            'requests>=2.8.1,<3.0.0',
+            'colorama>=0.3.3,<1.0.0',
+            'click>=5.1,<6.0',
+            'pygments>=2.0.2,<3.0.0',
+            'feedparser>=5.2.1,<6.0.0',
+            'pytz>=2016.3,<2017.0',
+            'docopt>=0.6.2,<1.0.0',
+            'uritemplate.py>=0.2.0,<1.0.0',
         ],
         skw['entry_points'] = {
             'pygments.lexers': ['gitsome = xonsh.pyghooks:XonshLexer',
