0bf4ea31e0fbc68fa5b5e1bf5afc6a256ab38bd2
Donne Martin <donne.martin@gmail.com>
2016-11-12 11:47:01 -0500

    Exclude .tox/ and .eggs/ from linter

--- scripts/run_code_checks.sh
+++ scripts/run_code_checks.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-flake8 --max-line-length=80 --exclude=build,docs,scratch,docs,html2text.py,compat.py,xonsh,lib,parser.table.py,parser.out,parser_test_table.py,parser_table.py,completions.py,completions_git.py,tests/data/* .
+flake8 --max-line-length=80 --exclude=build,docs,scratch,docs,html2text.py,compat.py,xonsh,lib,parser.table.py,parser.out,parser_test_table.py,parser_table.py,completions.py,completions_git.py,tests/data/*,.tox/,.eggs/ .
