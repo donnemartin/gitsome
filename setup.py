28a5c8638ba4853d4a0c2e11028e182e7c13de12
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2017-01-03 12:08:33 +0200

    Add python 3.6 (#103)

--- setup.py
+++ setup.py
@@ -86,6 +86,7 @@ def main():
             'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
             'Topic :: Software Development',
             'Topic :: Software Development :: Libraries :: Python Modules',
         ],
